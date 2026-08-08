02155168e5e03f7327b5687037b26abef34d093d
Synclon <retr0muhammad@gmaile.com>
2026-08-08 15:29:53 +0100

    add website and payload

--- host.py
+++ host.py
@@ -1,10 +1,16 @@
 import http.server
 import ssl
 
-server_address = ('0.0.0.0', 443)
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/public" or self.path == "/public/":
+            self.path = "/public/index.html"
+        super().do_GET()
+
+server_address = ("0.0.0.0", 443)
 context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-context.load_cert_chain('localhost.pem')
-httpd = http.server.HTTPServer(server_address,http.server.SimpleHTTPRequestHandler)
-httpd.socket = context.wrap_socket(httpd.socket,server_side=True)
-print('running server')
+context.load_cert_chain("localhost.pem")
+httpd = http.server.HTTPServer(server_address, Handler)
+httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
+print("running server")
 httpd.serve_forever()
